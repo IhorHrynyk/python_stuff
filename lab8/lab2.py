f6acd19a4b00059dae59b0caab65c9787b388c21
IhorHrynyk <grynyk_ihor@ukr.net>
2017-10-12 10:59:34 +0300

    task2 (insertion sorting)

---
+++ lab8/lab2.py
@@ -0,0 +1,18 @@
+# python
+# coding: utf-8
+
+def insertionSort(a):
+   for index in range(1,len(a)):
+
+     currentvalue = a[index]
+     position = index
+
+     while position > 0 and a[position - 1] > currentvalue:
+         a[position] = a[position - 1]
+         position = position - 1
+
+     a[position] = currentvalue
+
+myList = [54,26,93,17,77,31,44,55,20]
+insertionSort(myList)
+print(myList)
